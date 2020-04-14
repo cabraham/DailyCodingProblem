fdbd71e38b09d39a741d7c03469dea02a73b5278
Ciby Abraham <5863494+cabraham@users.noreply.github.com>
2020-04-13 22:32:41 -0400

    Adding problem 3

---
+++ DCP.Exercises/Problem 003/Problem.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCP.Exercises.Problem_003
+{
+    public class Problem
+    {
+        private readonly Node tree;
+
+        public Problem(Node tree)
+        {
+            this.tree = tree;
+        }
+
+        public string Serialize()
+        {
+            var stack = new Stack<Node>();
+            stack.Push(tree);
+            var s = "";
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+                if (node == null)
+                    s += "-|";
+                else
+                {
+                    s += node.Value + "|";
+                    stack.Push(node.Right);
+                    stack.Push(node.Left);
+                }
+            }
+
+            return s;
+        }
+
+        public Node Deserialize(string input)
+        {
+            var arr = input.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+            var queue = new Queue<string>(arr);
+
+            var node = DeserializeNode(queue);
+            return node;
+        }
+
+        private Node DeserializeNode(Queue<string> queue)
+        {
+            if (queue.Peek() != null)
+            {
+                var val = queue.Dequeue();
+                if (val == "-")
+                    return null;
+
+                var node = new Node(val);
+                node.Left = DeserializeNode(queue);
+                node.Right = DeserializeNode(queue);
+
+                return node;
+            }
+
+            return null;
+        }
+    }
+
+    public class Node
+    {
+        public Node(string value, Node left = null, Node right = null)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+
+        public string Value { get; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+}
---
+++ DCP.Tests/Problem 003/ProblemTests.cs
@@ -0,0 +1,49 @@
+﻿using DCP.Exercises.Problem_003;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCP.Tests.Problem_003
+{
+    /*
+ * Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), 
+ * which deserializes the string back into the tree.
+
+For example, given the following Node class
+
+class Node:
+def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+    The following test should pass:
+
+node = Node("root", Node("left", Node("left.left")), Node("right"))
+assert deserialize(serialize(node)).left.left.val == "left.left"
+ */
+
+    [TestFixture]
+    public class ProblemTests
+    {
+        private Problem sut;
+
+        [SetUp]
+        public void Given()
+        {
+            var node = new Node("root", new Node("left", new Node("left.left")), new Node("right"));
+            sut = new Problem(node);
+        }
+
+        [Test]
+        public void ReturnsCorrectValueWhenDeserialized()
+        {
+            var serialized = sut.Serialize();
+            var deserialized = sut.Deserialize(serialized);
+            Assert.AreEqual("left.left", deserialized.Left.Left.Value);
+        }
+
+
+    }
+}
